8e47e05995c58da728b8345ad85aaaaeccc7a3d4
LamontCranston20 <149571640+LamontCranston20@users.noreply.github.com>
2025-07-24 21:09:22 +0100

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    fs: {
-      allow: ['.'],
-    },
-    historyApiFallback: true, // 👈 THIS is what ensures routing works
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env': {
+        VITE_CLERK_PUBLISHABLE_KEY: JSON.stringify(env.VITE_CLERK_PUBLISHABLE_KEY)
+      }
+    }
+  };
 });
